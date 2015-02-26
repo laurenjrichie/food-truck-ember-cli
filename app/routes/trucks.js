90b658fd1f87926b46609e69ae6c43d2b1a7e9c0
laurenjrichie <laurenjrichie@gmail.com>
2015-02-25 21:13:52 -0800

    map loading upon page load

--- app/routes/trucks.js
+++ app/routes/trucks.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  activate: function() {
+    setTimeout(function() {
+      L.mapbox.accessToken = 'pk.eyJ1IjoibGF1cmVuanJpY2hpZSIsImEiOiJHTEY1OVFZIn0.MlJXkQlI1fW9t4Yi3ZOYlg';
+      var map = L.mapbox.map('map', 'laurenjrichie.ladp904d'); // can use .setView here to specify coordinates
+      
+      var geojson = [{
+                      "type": "FeatureCollection",
+                      "features": [
+                        {
+                          "type": "Feature",
+                          "geometry": {
+                            "type": "Point",
+                            "coordinates": [-122.388078331989, 37.7667867792956]
+                          },
+                          "properties": {}
+                        },
+                      ]
+                    }];
+      
+      var myLayer = L.mapbox.featureLayer().addTo(map);
+      return myLayer.setGeoJSON(geojson);
+    }, 200);
+    
+  }
 });
